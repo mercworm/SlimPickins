37e563f606796c97ab0aa7615dfcbdffe9abed8f
Beck <1008887@student.sae.edu.au>
2018-12-18 19:48:13 +1000

    fixed player movement - functionality only. it's bad also fixed up the idle animation for walking

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -13,8 +13,10 @@ public class PlayerMovement : MonoBehaviour {
     public Transform groundCheck;
     public float jumpForce;
 
+    public float maxSpeed;
+
     [HideInInspector] public bool facingRight = true;
-    [HideInInspector] public bool jump = true;
+    [HideInInspector] public bool jump = false;
     [HideInInspector] public bool canMove = true;
 
     [Header("PlayerType")]
@@ -27,9 +29,13 @@ public class PlayerMovement : MonoBehaviour {
     public AudioSource source;
     public AudioClip soundLand, soundJump, soundPeck, soundWalk;
 
+    [Header("Animation")]
+    public Animator legAnim;
+    public Animator headAnim;
+
     private void Start()
     {
-        rb2d.GetComponent<Rigidbody2D>();
+        rb2d = GetComponent<Rigidbody2D>();
 
         speed = playerType.speed;
         peckSpeed = playerType.peckSpeed;
@@ -39,6 +45,7 @@ public class PlayerMovement : MonoBehaviour {
     private void Update()
     {
         grounded = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
+        Debug.Log(grounded);
 
         if (Input.GetKeyDown(keyJump) && grounded)
         {
@@ -50,14 +57,34 @@ public class PlayerMovement : MonoBehaviour {
     {
 		if(canMove)
         {
-            if(Input.GetKey(keyLeft))
-            {
+            float h = Input.GetAxis("Horizontal");
+            legAnim.SetFloat ("Speed", Mathf.Abs (h));
 
-            }
-            if(Input.GetKey(keyRight))
-            {
+            if (h * rb2d.velocity.x < maxSpeed)
+                rb2d.AddForce(Vector2.right * h * speed);
 
+            if (Mathf.Abs(rb2d.velocity.x) > maxSpeed)
+                rb2d.velocity = new Vector2(Mathf.Sign(rb2d.velocity.x) * maxSpeed, rb2d.velocity.y);
+
+            if (h > 0 && !facingRight)
+                Flip();
+            else if (h < 0 && facingRight)
+                Flip();
+
+            if (jump)
+            {
+                //anim.SetTrigger ("Jump");
+                rb2d.AddForce(new Vector2(0f, jumpForce));
+                jump = false;
             }
         }
 	}
+
+    public void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
 }
